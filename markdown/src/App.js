64f05fae8d95a367140cc1085eaad13695e6d039
Lucas Melin <65817583+sophonTBP@users.noreply.github.com>
2021-05-17 20:06:20 +0200

    test

---
+++ markdown/src/App.js
@@ -0,0 +1,49 @@
+
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ReactDom from 'react-dom'
+import unified from 'unified'
+import parse from 'remark-parse'
+import remark2react from 'remark-react'
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    // Change code below this line
+this.handleChange=this.handleChange.bind(this)
+    // Change code above this line
+  }
+  // Change code below this line
+
+handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+}
+  
+  render() {
+    return (
+      <div class= "container">
+       <header id="header" >React Markdown previewer</header>
+<textarea value = {this.state.input} onChange = {this.handleChange} id="editor"></textarea>
+        
+        
+
+        <div id="preview"><div>previewer:</div>{
+        unified()
+        .use(parse)
+        .use(remark2react)
+        .processSync(this.state.input).result
+        }</div>
+        <footer id="footer">this project uses Remark to sanitize input</footer>
+      </div>
+    );
+  }
+};
+
+export default App;
