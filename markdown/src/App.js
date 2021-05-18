136847e12c0f2b42e253e91f95fbe3d6309d6661
Lucas Melin <65817583+sophonTBP@users.noreply.github.com>
2021-05-18 02:06:24 +0200

    working version1.0

--- markdown/src/App.js
+++ markdown/src/App.js
@@ -1,18 +1,18 @@
 
-
+import './logo.svg';
 import './App.css';
 import React from 'react';
-
+import ReactFCCtest from 'react-fcctest';
 import unified from 'unified'
 import parse from 'remark-parse'
 import remark2react from 'remark-react'
-
+var breaks = require('remark-breaks')
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: ''
+      input:placeholder
     };
     // Change code below this line
 this.handleChange=this.handleChange.bind(this)
@@ -28,22 +28,67 @@ handleChange(event) {
   
   render() {
     return (
+      
       <div class= "container">
-       <header id="header" >React Markdown previewer</header>
-<textarea value = {this.state.input} onChange = {this.handleChange} id="editor"></textarea>
+        <ReactFCCtest />
+       <h3 id="header" >React Markdown previewer</h3>
+<textarea  value = {this.state.input} onChange = {this.handleChange} id="editor"></textarea>
         
         
 
-        <div id="preview"><div>previewer:</div>{
+        <div id="preview">{
         unified()
         .use(parse)
         .use(remark2react)
+        .use(breaks)
         .processSync(this.state.input).result
         }</div>
         <footer id="footer">this project uses Remark to sanitize input</footer>
       </div>
+      
     );
   }
 };
 
 export default App;
+const placeholder = `# Welcome to my React Markdown Previewer! 
+# h1 title 
+## h2 title
+[a link](url),
+
+some code\`<div></div>\`
+
+\`\`\`
+git status
+git add 
+git commit
+\`\`\`
+
+An unordered list:
+- George Washington
+- John Adams
+- Thomas Jefferson
+
+A blockquote:
+>The missile knows where it is at all times. 
+>It knows this because it knows where it isn't,
+>by subtracting where it is, from where it isn't,
+>or where it isn't, from where it is, whichever is greater, 
+>it obtains a difference, or deviation. 
+>The guidance sub-system uses deviations 
+>to generate corrective commands to drive the missile 
+>from a position where it is, to a position where it isn't,
+>and arriving at a position where it wasn't, it now is.
+
+An image: 
+![logo](logo.svg) 
+
+**bold** text.`
+
+
+
+
+
+
+
+
